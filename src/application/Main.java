13c2b475eb1a9a1fdd308a2756331d54fc821927
ahmedbq <ahmedbq1993@gmail.com>
2020-01-26 03:49:09 -0500

    Added initial GUI

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,48 @@
 package application;
 	
+import java.util.EnumSet;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			BorderPane borderPane = new BorderPane();
+			GridPane gridPane = new GridPane();
+			
+			// Create all of the input text fields
+			EnumSet.allOf(INPUT.class)
+				   .forEach(e -> {
+					   TextField text = new TextField();
+					   text.setTooltip(new Tooltip(e.toString()));
+					   
+					   gridPane.add(text, e.ordinal(), 1);
+					   
+					   Label desc = new Label();
+					   desc.setText(e.toString());
+					   
+					   gridPane.add(desc, e.ordinal(), 2);
+					   GridPane.setHalignment(desc, HPos.CENTER);
+				   });
+			
+			
+			
+			borderPane.setCenter(gridPane);
+			Scene scene = new Scene(borderPane,800,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.setFill(Color.rgb(181,198,145));
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -22,5 +52,13 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public enum INPUT {
+		PRICE,
+		COUPON,
+		YEARS,
+		FACE,
+		RATE
 	}
 }
